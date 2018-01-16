e7580e7963786a46f0e00821320c6eca1a154e7e
Marcin Pudełek <mrcnpdlk@gmail.com>
2018-01-16 20:31:06 +0100

    dir schema

---
+++ src/ImageWebTool/Request.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Marcin.
+ * Date: 16.01.2018
+ * Time: 20:30
+ */
+
+namespace mrcnpdlk\ImageWebTool;
+
+
+class Request
+{
+
+}
---
+++ src/api.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Created by Marcin.
+ * Date: 16.01.2018
+ * Time: 20:29
+ */
