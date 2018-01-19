74bd343115bae3e16c8502c1c0ad52106de0ca3c
Marcin Pudełek <mrcnpdlk@gmail.com>
2018-01-20 00:12:57 +0100

    Config

--- api/api.php
+++ api/api.php
@@ -4,7 +4,6 @@ use mrcnpdlk\ImageWebTool\Bootstrap;
 use mrcnpdlk\ImageWebTool\Config;
 use mrcnpdlk\ImageWebTool\FileHandler;
 use mrcnpdlk\ImageWebTool\Helper;
-use phpFastCache\Helper\Psr16Adapter;
 use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -15,20 +14,7 @@ require __DIR__ . '/../vendor/autoload.php';
  * API Configuration
  */
 Helper::setConfig(
-    new Config([
-            'storage'       => __DIR__ . '/../content',
-            'font'          => __DIR__ . '/../fonts/blowbrush/blowbrush.ttf',
-            'cacheInstance' => new Psr16Adapter(
-                'redis',
-                [
-                    "host"                => null, // default localhost
-                    "port"                => null, // default 6379
-                    'defaultTtl'          => 3600 * 24, // 24h
-                    'ignoreSymfonyNotice' => true,
-                ]),
-            'debug'         => true,
-        ]
-    )
+    new Config([__DIR__.'/../config/imagewebtool.ini'])
 );
 
 
@@ -45,18 +31,26 @@ $app->get('/{version}/{params}[/{file}]', function (Request $request, Response $
 
     $oBootstrap = new Bootstrap($request, $args);
     /**
-     * @var string $imageBlob
+     * @var string                         $imageBlob
+     * @var \mrcnpdlk\Lib\PfcAdapter\Cache $oCache
      */
-    $imageBlob = Helper::getFromCache(
-        Helper::getConfig()->get('cacheInstance'),
-        function () use ($oBootstrap) {
-            $oFile = new FileHandler($oBootstrap->getFileName(), $oBootstrap->getParams());
+    $oCache    = Helper::getConfig()->get('cacheInstance');
+    $imageBlob = $oCache
+        ->set(
+            function () use ($oBootstrap) {
+                $oFile = new FileHandler(
+                    $oBootstrap->getFileName(),
+                    $oBootstrap->getParams());
 
-            return $oFile->getBlob();
-        },
-        $oBootstrap->getHash(),
-        3600 * 24 // 24h cache
-    );
+                return $oFile->getBlob();
+            },
+            $oBootstrap->getHash(),
+            null,
+            1
+        )
+        ->get()
+    ;
+
     $response->write($imageBlob);
 
     return $response->withHeader('Content-Type', Helper::getMimeFromBlob($imageBlob));
--- src/ImageWebTool/Config.php
+++ src/ImageWebTool/Config.php
@@ -8,22 +8,36 @@
 namespace mrcnpdlk\ImageWebTool;
 
 
-use Noodlehaus\AbstractConfig;
+use mrcnpdlk\Lib\PfcAdapter\Cache;
+use phpFastCache\CacheManager;
 
 /**
  * Class Config
  *
  * @package mrcnpdlk\ImageWebTool
  */
-class Config extends AbstractConfig
+class Config extends \Noodlehaus\Config
 {
+    /**
+     * @return array
+     */
     protected function getDefaults(): array
     {
+        $config = [
+            'host'                => null,
+            'port'                => null,
+            'defaultTtl'          => 3600 * 24, // 24h
+            'ignoreSymfonyNotice' => true,
+            'fallback'            => 'files',
+        ];
+
+        $oCache = new Cache(CacheManager::Redis($config));
+
         return [
             'storage'       => \dirname(__DIR__, 2) . '/content',
             'font'          => \dirname(__DIR__, 2) . '/fonts/blowbrush/blowbrush.ttf',
-            'cacheInstance' => null,
-            'debug'         => false,
+            'cacheInstance' => $oCache,
+            'debug'         => true,
         ];
     }
 
